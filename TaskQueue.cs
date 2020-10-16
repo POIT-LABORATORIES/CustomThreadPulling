4983adcbc1967a9c7dc4611d6e8ab280ede9e534
Pluralment <Foolman.ru@yandex.ru>
2020-10-16 11:19:41 +0300

    First push

---
+++ TaskQueue.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomThreadPulling
+{
+    public class TaskQueue
+    {
+        // Содержит очередь задач в виде делегатов без параметров.
+        public TaskQueue()
+        {
+            // Создание указанного количества потоков пула.
+        }
+    }
+}
